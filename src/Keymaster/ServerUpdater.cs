7077b1ae66c0691ceb9320c5bbcd663397110f0b
anton <astruyk@gmail.com>
2014-11-28 16:54:42 -0500

    Mostly done generating data for server updates.

--- src/Keymaster/ServerUpdater.cs
+++ src/Keymaster/ServerUpdater.cs
@@ -1,9 +1,11 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
@@ -61,31 +63,136 @@ namespace Gatekeeper
 			Log("Starting...");
 			WebClient client = new WebClient();
 			StartNewPhase(Phase.DownloadingMappingFile);
-			dynamic keyMappingJson = JObject.Parse(client.DownloadString(_settings.KeyMappingFileUrl));
+			Log(LogLevel.Debug, "Downloading {0}", _settings.KeyMappingFileUrl);
+			var keyMappingFileContents = client.DownloadString(_settings.KeyMappingFileUrl);
+			var keyMappings = JsonConvert.DeserializeObject<Dictionary<string, string[]>>(keyMappingFileContents);
+			keyMappings = new Dictionary<string,string[]>(keyMappings, StringComparer.CurrentCultureIgnoreCase);
 
 			StartNewPhase(Phase.DownloadingPlayWithSixConfig);
-			string playWithSixConfigContents = client.DownloadString(_config.PlayWithSixConfigFileUrl);
+			Log(LogLevel.Debug, "Downloading {0}", _config.PlayWithSixConfigFileUrl);
+			string playWithSixConfigFileContents = client.DownloadString(_config.PlayWithSixConfigFileUrl);
 
 			StartNewPhase(Phase.DownloadingParFile);
+			Log(LogLevel.Debug, "Downloading {0}", _config.ParFileSourceUrl);
 			string parFileContents = client.DownloadString(_config.ParFileSourceUrl);
 
 			StartNewPhase(Phase.ProcessingDownloadedData);
+			
+			// Grab the list of mods in the play with six file.
+			var playWithSixMods = GetModsFromPlayWithSixFile(playWithSixConfigFileContents);
+			Log("Found {0} keys in play with six mod list.", playWithSixMods.Count);
+			foreach (var mod in playWithSixMods)
+			{
+				Log("\t{0}", mod);
+			}
+
+			// Generate the list of mods that we need to install of the server
+			var modsForServer = playWithSixMods.Except(_settings.ClientOnlyModList);
+			Log("Found {0} mod(s) that need to be running on the server.", modsForServer.Count());
+			foreach (var mod in modsForServer)
+			{
+				Log("\t{0}", mod);
+			}
+
+			// Generate the command line we need to run to start the server
+			var modCommandLine = string.Format("-mod={0}",  String.Join(";", modsForServer));
+			var parFileModCommand = string.Format(@"mod=""-mod={0}"";", modCommandLine);
+			Log("Generated mod command-line args for server.");
+			Log(LogLevel.Debug, modCommandLine);
+
+			// Generate a mapping of the keys we need to install to the list of mods
+			// that require them (for reporting)
+			var keysToInstall = new Dictionary<string, List<string>>(StringComparer.CurrentCultureIgnoreCase);
+			foreach (var modName in playWithSixMods.Concat(_settings.ManualMods))
+			{
+				string[] requiredKeys;
+				if (keyMappings.TryGetValue(modName, out requiredKeys))
+				{
+					foreach (var key in requiredKeys)
+					{
+						List<string> modsRequestingKey;
+						if (keysToInstall.TryGetValue(key, out modsRequestingKey))
+						{
+							modsRequestingKey.Add(modName);
+						}
+						else
+						{
+							keysToInstall[key] = new List<string> { modName };
+						}
+					}
+				}
+				else
+				{
+					Log(LogLevel.Error, "Failed to find mod list entry for '{0}'. Unable to continue", modName);
+					return;
+				}
+			}
+
+			// Add the manually required keys	
+			foreach (var keyName in _settings.ManualKeys)
+			{
+				List<string> modsRequestingKey;
+				if (keysToInstall.TryGetValue(keyName, out modsRequestingKey))
+				{
+					modsRequestingKey.Add("MANUAL");
+				}
+				else
+				{
+					keysToInstall[keyName] = new List<string> { "MANUAL" };
+				}
+			}
+
+			Log("Found {0} keys that need to be installed on the server.", keysToInstall.Count());
+			foreach(var entry in keysToInstall)
+			{
+				Log("\t{0} -> {{ {1} }}", entry.Key, String.Join(",", entry.Value));
+			}
+
 			Log("Done!");
 			Log("");
 			Log("Successfully updated server!");
 		}
 
+		private List<string> GetModsFromPlayWithSixFile(string playWithSixConfigFileContents)
+		{
+			var modList = new List<string>();
+
+			var modRegex = new Regex(@"\s*-\s*""(@\w+)""");
+			bool isInModList = false;
+			foreach(var line in playWithSixConfigFileContents.Split(new string [] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries))
+			{
+				if (isInModList && line.StartsWith(":"))
+				{
+					isInModList = false;
+				}
+				if (line.Contains(":required_mods:") || line.Contains(":allowed_mods:"))
+				{
+					isInModList = true;
+				}
+				if (isInModList)
+				{
+					var match = modRegex.Match(line);
+					if (match.Success)
+					{
+						modList.Add(match.Groups[1].Value);
+					}
+				}
+			}
+
+			return modList;
+		}
+
 		private void StartNewPhase(Phase newPhase)
 		{
-			Log (LogLevel.Message, "Changing phase from {0} to {1}", CurrentPhase, newPhase);
-			CurrentPhase = newPhase;
-			
+			Log("Finished {0} phase.", CurrentPhase);
+			Log ("Starting '{0}' phase.", newPhase);
+			CurrentPhase = newPhase;	
 			if (PhaseChanged != null) { PhaseChanged(this, new EventArgs()); }
 		}
 
 		private void Log(string format, params object[] args)
 		{
-			Log(LogLevel.Debug, format, args);
+			Log(LogLevel.Message, format, args);
 		}
 
 		private void Log(LogLevel level, string format, params object[] args)
@@ -93,8 +200,8 @@ namespace Gatekeeper
 			var message = String.Format(format, args);
 			switch(level)
 			{
-				case LogLevel.Debug: { message = "[Debug] " + message; break; }
-				case LogLevel.Error: { message = "[ERROR] " + message; break; }
+				case LogLevel.Debug: { message = "[Debug]\t" + message; break; }
+				case LogLevel.Error: { message = "[ERROR]\t" + message; break; }
 			}
 			if (OutputGenerated != null)
 			{
